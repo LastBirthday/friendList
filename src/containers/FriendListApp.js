f3d20c68f27bdc58b517eb1123f531f3edbd68dc
LastBirthday <dmitrienko92@gmail.com>
2017-03-05 15:25:01 +0300

    Starting configuratoin

---
+++ src/containers/FriendListApp.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Dds on 05.03.2017.
+ */
+
+import React, { Component } from 'react';
+
+export default class FriendListApp extends React.Component {
+    render() {
+        return (
+            <h1>Hello, world!</h1>
+        );
+    }
+}
